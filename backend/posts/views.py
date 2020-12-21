0e035f64d50883addb5b0fe42b70c6e55696f96d
s3855825 <s3855825@rmit.edu.vn>
2020-12-21 15:31:51 +0700

    change post view

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -24,6 +24,7 @@ class PostView(APIView):
     def post(self, request, format=None):
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
+            print('POST SERIALIZER DATA:', serializer.validated_data)
             serializer.save()
             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
