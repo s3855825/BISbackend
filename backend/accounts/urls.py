6e4e374d50bda959f24fd4ad79c0c35e219ef69d
s3855825 <s3855825@rmit.edu.vn>
2021-01-06 10:32:03 +0700

    fix notification app

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('<int:primary_key>/posts/', views.UserPostView.as_view(), name='users_post'),
     path('<int:primary_key>/groups/', views.UserGroupView.as_view(), name='users_group'),
     path('<int:primary_key>/reviews/', views.UserReviewView.as_view(), name='users_review'),
+    path('<int:primary_key>/notifications/', views.UserNotificationView.as_view(), name='users_noti'),
+    path('<int:primary_keys>/sent_requests/', views.SenderRequestView.as_view(), name='users_sent_requests'),
+    path('<int:primary_keys>/received_requests/', views.ReceiverRequestView.as_view(), name='users_received_requests'),
 ]
--- backend/notifications/serializers.py
+++ backend/notifications/serializers.py
@@ -3,18 +3,6 @@ from rest_framework import serializers
 from .models import Notification
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ("id", "username", "email", "password", "friendcode", )
-        read_only_fields = ("is_active",)
-        # extra_kwargs = {'password': {'write_only': True}}
-
-    def create(self, validated_data):
-        user = super().create(validated_data)
-        user.save()
-        return user
-
 class NotificationSerializer(serializers.Serializer):
     class Meta:
         model = Notification
