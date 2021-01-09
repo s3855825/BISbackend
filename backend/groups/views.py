37d5f0e4a6cdaf2de5ecb5ebc718dac1779b4ed5
s3855825 <s3855825@rmit.edu.vn>
2021-01-09 16:56:54 +0700

    fix add task to group

--- backend/groups/views.py
+++ backend/groups/views.py
@@ -189,7 +189,7 @@ class GroupTaskView(APIView):
         gt_serializer = GroupTaskSerializer(data=gt_serializer_data)
         if gt_serializer.is_valid(raise_exception=True):
             gt_serializer.save()
-            return Response(data={}, status=status.HTTP_200_OK)
+            return Response(data=gt_serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(gt_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
